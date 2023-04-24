a650b4b0bc8d87639abf4567f31d80106f1566ac
zzh <853818394@qq.com>
2023-04-24 14:45:58 +0800

    3.普通Maven项目改造为SpringBoot项目.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,15 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication //指定当前项目为一个springboot项目
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!123321666");
+
+        //作为一个Spring项目运行
+        SpringApplication.run(Main.class,args);
+
 
     }
 }
