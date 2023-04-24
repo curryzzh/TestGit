5bf51fa885264efe9610e489d28971a509a38d95
zzh <853818394@qq.com>
2023-04-24 15:22:31 +0800

    4.添加测试类

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MainTest {
+    @Test
+    public void test(){
+        System.out.println("Hello Test");
+    }
+}
